1b42b5ebea884855d1dca2f211c890dfdbb1d0e3
roniest360 <130781091+roniest360@users.noreply.github.com>
2024-01-18 10:41:17 -0600

    Update particles.js

--- static/assets/scripts/particles.js
+++ static/assets/scripts/particles.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
     "particles": {
       "number": {
-        "value": 200,
+        "value": 2000,
         "density": {
           "enable": true,
           "value_area": 600
